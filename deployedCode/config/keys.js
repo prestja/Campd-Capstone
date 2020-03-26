59f661dc5e77981e79e2b89862f657d239cfc1a3
Dominic Whiting <54961251+domvrk@users.noreply.github.com>
2020-03-26 14:01:41 -0600

    changed mongodb server

--- deployedCode/config/keys.js
+++ deployedCode/config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb+srv://dom:dom@domvrk-lcuap.mongodb.net/test?retryWrites=true&w=majority",
+  mongoURI: "mongodb+srv://groupAccount:Rappcampdcapstone2020@cluster0-0zrg0.gcp.mongodb.net/test?retryWrites=true&w=majority",
   secretOrKey: "sercret"
 };
