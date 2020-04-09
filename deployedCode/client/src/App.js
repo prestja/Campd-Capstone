623d74883890d9255d3ee91179dfac413316916f
Colton <colton.estes12@gmail.com>
2020-04-09 00:40:56 -0500

    Cleaned up files and made small UI tweak

--- deployedCode/client/src/App.js
+++ deployedCode/client/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
-import Register from "./components/auth/Register";
+import Register from "./components/auth/AddUser";
 import Login from "./components/auth/Login";
 import Projects from "./components/layout/Projects"
 import Signup from "./components/layout/Signup"
