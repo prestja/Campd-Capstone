2b5141fb24e4fd8c18c268c7b461abba88f2f023
Jacob Preston <prestja@gmail.com>
2020-10-14 11:17:58 -0500

    Folded functionality of link into project title

--- client/src/components/layout/Listing.js
+++ client/src/components/layout/Listing.js
@@ -7,18 +7,19 @@ import undraw from '../../images/projects.svg';
 class Listing extends React.Component {
 	render() {
 		return (
-			<SimpleGrid columns = {2} pl = "40px" pr = "40px">
+			<SimpleGrid columns = {2} pl = "1vw" pr = "1vw">
 				<Box>
-					<AspectRatioBox maxW = "800px" ratio = {1 / 1}>
+					<AspectRatioBox maxW = "30vw" ratio = {1 / 1}>
 						<Image src = {undraw}/>
 					</AspectRatioBox>
 				</Box>
 				<Box>
-					<Text fontSize = "2xl"><b>{this.props.name}</b></Text>
+					<Link to = {{pathname: 'project/' + this.props._id, state: {name: this.props.name, description: this.props.description}}}>
+						<Text fontSize = "xl">{this.props.name}</Text>
+					</Link>
 					<Text fontSize = "sm"><i>Created by {this.props.owner}</i></Text>
 						<Text>{this.props.description}</Text>
 					<Text>Status: {this.props.status}</Text>
-					<Link to = {{pathname: 'project/' + this.props._id, state: {name: this.props.name, description: this.props.description}}}>View Project</Link>
 				</Box>
 			</SimpleGrid>
 		);
