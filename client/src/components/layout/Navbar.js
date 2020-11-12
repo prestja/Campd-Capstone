10e615326b8f47db3253bdbc586270f5dbaa8802
Jacob Preston <prestja@gmail.com>
2020-11-12 16:59:47 -0600

    Reworked navbar to consist of tabs instead of plain links

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import {
 	Flex,
-	Link,
 	Image,
 	Grid,
 	Text,
+	TabList,
+	Tab,
+	Tabs,
 } from "@chakra-ui/core";
 
 class Navbar extends Component {
@@ -17,21 +19,43 @@ class Navbar extends Component {
 
 	render() {
 		const isLoggedIn = true;
-		const isAdmin = true; 
+		const isAdmin = true;
+		const tabData = [
+			{
+				  label: "Home",
+				  href: "/",
+			},
+			{
+				  label: "Projects",
+				  href: "/projects",
+			},
+		];
 		return (
-			<Grid templateColumns="repeat(auto-fit, 60vw)" gap={6} bg = "untaccentgray.700" color = "white" justifyContent="center">
-				<Flex>
-					<Image size = "5vw" src={require('./unt-banner.svg')}></Image>
-					<Text fontWeight = "bold" fontSize = {{base: "xl", sm: "3xl", md: "4xl"}} color = "white" textAlign= "center">UNT Greenlight Projects Portal</Text>
-				</Flex>
-				<Flex>
-					<Link marginRight = "1rem" color = "white" href ="/">Home</Link >
-					<Link marginRight = "1rem" color = "white" href  = "/projects">Projects</Link>
-					{!isLoggedIn ? null: <Link marginRight = "1rem" color = "white" href="/AddProject">Add Project</Link>}
-					{!isLoggedIn ? <div><Link marginRight = "1rem" color = "white" href="/login">Login</Link> <Link marginRight = "1rem" color = "white" to="/signup">Sign up</Link> </div>: <Link marginRight = "1rem" color = "white" href = "/profile">Profile</Link>}
-					{isAdmin && isLoggedIn ? <Link marginRight = "1rem" color = "white" href="/admin">Admin</Link> : null}
-				</Flex>
-			</Grid>
+			<Flex bg = "white">
+				<Image size = "5vw" src={require('./unt-banner.svg')}></Image>
+				<Grid templateColumns="repeat(auto-fit, 60vw)" gap={6} color = "untsec1" justifyContent="center">
+					{/* First row */}
+					<Flex>
+						<Text fontWeight = "bold" fontSize = {{base: "xl", sm: "3xl", md: "4xl"}}>UNT Greenlight Projects Portal</Text>
+					</Flex>
+					{/* Second row*/}
+					<Flex>
+						<Tabs
+							isFitted
+							variant = "enclosed"
+							onChange={(index) => { {/* */}
+								window.location.href = tabData[index].href;
+							}}>
+							<TabList>
+								{tabData.map((tab, index) => (
+									<Tab key={index}>{tab.label}</Tab>
+								))}
+							</TabList>
+						</Tabs>
+					</Flex>
+				</Grid>
+			</Flex>
+			
 		);
 	}
 }
