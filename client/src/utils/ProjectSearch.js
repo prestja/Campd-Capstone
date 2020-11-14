26f2cfa142b5d821979acdb0cf537cf4e876fc97
Jacob Preston <prestja@gmail.com>
2020-11-14 16:35:21 -0600

    Fixes issue where project filters would not correctly toggle

--- client/src/utils/ProjectSearch.js
+++ client/src/utils/ProjectSearch.js
@@ -17,19 +17,43 @@ import {
 
 class SearchBar extends Component {
 	state = {
-		value: ""
+		filters: {
+			new: false,
+			recruiting: false,
+			active: false,
+			paused: false,
+			stopped: false,
+			archived: false,
+			proposals: false
+		}
 	};
 
+	componentDidMount() {
+		console.log("componenetDidMOutn");
+		this.setState({
+			...this.state,
+			filters: {
+				new: this.props.new,
+				recruiting: this.props.recruiting,
+				active: this.props.active,
+				paused: this.props.paused,
+				stopped: this.props.stopped,
+				archived: this.props.archived,
+				proposals: this.props.proposals
+			}
+		});
+		console.log(this.state);
+		console.log(this.props);
+
+	}
+	
 	handleSearchChange = e => {
 		this.setState({
-			value: e
+			...this.state,
+			terms: e,
 		});
 		this.handleSubmit(e);
 	};
-
-	handleStatusChange = e => {
-
-	}
 
 	handleSubmit = e => {
 		this.props.onSearch(e.toLowerCase());
@@ -42,9 +66,10 @@ class SearchBar extends Component {
 		this.handleSearchChange(e);
 	};
 
-	handleSwitch = (n, b) => {
-		console.log(n);
-		console.log(b);
+	handleSwitch = (n) => {
+		let copy = this.state;
+		copy.filters[n] = !copy.filters[n];
+		this.setState(copy);
 	};
 
 	render() {
@@ -53,20 +78,20 @@ class SearchBar extends Component {
 				<Editable 
 					placeholder ="Enter project search terms here"
 					type = "value"
-					value = {this.state.value}
+					value = {this.state.terms}
 					onChange = {this.handleSearchChange}>
 					<EditablePreview />
 					<EditableInput />
 				</Editable>
 				<Divider></Divider>
 				<Stack direction = "row" d= "flex">
-					<FormLabel>Show: New</FormLabel><Switch colorScheme="blue" defaultIsChecked={this.props.new} onChange={(e) => this.handleSwitch("news", true)}/>
-					<FormLabel>Recruiting</FormLabel><Switch colorScheme="teal" defaultIsChecked={this.props.recruiting} onChange={(e) => this.handleSwitch("recruiting", true)}/>
-					<FormLabel>Active</FormLabel><Switch colorScheme="green" defaultIsChecked={this.props.active} onChange={(e) => this.handleSwitch("active", true)}/>
-					<FormLabel>Paused</FormLabel><Switch colorScheme="yellow" defaultIsChecked={this.props.paused} onChange={(e) => this.handleSwitch("paused", true)}/>
-					<FormLabel>Stopped</FormLabel><Switch colorScheme="orange" defaultIsChecked={this.props.stopped} onChange={(e) => this.handleSwitch("stopped", true)}/>
-					<FormLabel>Archived</FormLabel><Switch colorScheme="red" defaultIsChecked={this.props.archived} onChange={(e) => this.handleSwitch("archived", true)}/>
-					<FormLabel>Proposals</FormLabel><Switch colorScheme="gray" defaultIsChecked={this.props.proposals} onChange={(e) => this.handleSwitch("proposals", true)}/>
+					<FormLabel>Show: New</FormLabel><Switch colorScheme="blue" isChecked={this.state.filters.new} onChange={(e) => this.handleSwitch("new")}/>
+					<FormLabel>Recruiting</FormLabel><Switch colorScheme="teal" isChecked={this.state.filters.recruiting} onChange={(e) => this.handleSwitch("recruiting")}/>
+					<FormLabel>Active</FormLabel><Switch colorScheme="green" isChecked={this.state.filters.active} onChange={(e) => this.handleSwitch("active")}/>
+					<FormLabel>Paused</FormLabel><Switch colorScheme="yellow" isChecked={this.state.filters.paused} onChange={(e) => this.handleSwitch("paused")}/>
+					<FormLabel>Stopped</FormLabel><Switch colorScheme="orange" isChecked={this.state.filters.stopped} onChange={(e) => this.handleSwitch("stopped")}/>
+					<FormLabel>Archived</FormLabel><Switch colorScheme="red" isChecked={this.state.filters.archived} onChange={(e) => this.handleSwitch("archived")}/>
+					<FormLabel>Proposals</FormLabel><Switch colorScheme="gray" isChecked={this.state.filters.proposals} onChange={(e) => this.handleSwitch("proposals")}/>
 				</Stack>
 			</Stack>
 		);
